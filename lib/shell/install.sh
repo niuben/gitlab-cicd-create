080372177fa9e9545bccb5661a82237a3af99f7b
niuben <741001695@qq.com>
2020-06-01 14:05:23 +0800

    增加node-sass镜像

--- lib/shell/install.sh
+++ lib/shell/install.sh
@@ -5,6 +5,10 @@ npm config set registry https://registry.npm.taobao.org
 
 npm config set phantomjs_cdnurl https://npm.taobao.org/dist/phantomjs
 
+npm config set sass_binary_site https://npm.taobao.org/mirrors/node-sass/
+
+npm config set electron_mirror https://npm.taobao.org/mirrors/electron/
+
 # yarn install --ignore-engines --frozen-lockfile
 
 npm ci
